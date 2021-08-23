8685e95b579b47f7d289f61fe921c9b87e1132b9
Kicha <59894683+Kicha9992002@users.noreply.github.com>
2021-08-23 21:55:41 +0200

    ManufacturingComponent: Add factory

--- src/app/manufacturing/manufacturing.component.ts
+++ src/app/manufacturing/manufacturing.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map, take, tap } from 'rxjs/operators';
+
+import { Factory, Location, PropertyType } from '../shared/factory.model';
+import * as fromApp from '../store/app.reducer';
+import * as ManufacturingActions from './store/manufacturing.actions';
 
 @Component({
   selector: 'app-manufacturing',
@@ -6,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManufacturingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
+  }
+
+  onAddFactory() {
+    this.store.select('manufacturing').pipe(
+      take(1),
+      map(manufacturingState => manufacturingState.factories),
+      tap(factories => {
+        let maxId = factories[factories.length - 1].id;
+        this.store.dispatch(ManufacturingActions.addFactory({factory: new Factory(
+          maxId + 1,
+          'Apple orchard',
+          'https://cdn.pixabay.com/photo/2019/02/24/13/05/apple-icon-4017545_1280.png',
+          10,
+          Location.Germany,
+          PropertyType.owner
+        )}));
+      })
+    ).subscribe();
+
   }
 
 }
