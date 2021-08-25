8ae8f992d8e40f768cd318f8a0c43caca28465a6
Kicha <59894683+Kicha9992002@users.noreply.github.com>
2021-08-25 19:34:20 +0200

    Refactoring

--- src/app/header/money/store/money.actions.ts
+++ src/app/header/money/store/money.actions.ts
@@ -1,54 +1,24 @@
 import { createAction, props } from '@ngrx/store';
 
-export const setMoney = createAction(
-    '[Money] Set Money',
-    props<{
-        money: number
-    }>()
-);
-
-export const addMoney = createAction(
-    '[Money] Add Money',
-    props<{
-        money: number
-    }>()
-);
-
-export const subtractMoney = createAction(
-    '[Money] Subtract Money',
-    props<{
-        money: number
-    }>()
-);
-
-export const fetchMoney = createAction(
-    '[Money] Fetch Money'
-);
-
-export const payAddFactorySuccess = createAction(
-    '[Money] Pay Add Factory Success',
-    props<{
-        cost: number
-    }>()
-);
-
-export const payAddFactoryFail = createAction(
-    '[Money] Pay Add Factory Fail',
-    props<{
-        error: string
-    }>()
-);
-
-export const payAddFactorySizeSuccess = createAction(
-    '[Money] Pay Add Factory Size',
-    props<{
-        cost: number
-    }>()
-);
-
-export const payAddFactorySizeFail = createAction(
-    '[Money] Pay Add Factory Size Fail',
-    props<{
-        error: string
-    }>()
-);
+export const setMoney = createAction('[Money] Set Money', props<{
+    money: number
+}>());
+export const addMoney = createAction('[Money] Add Money', props<{
+    money: number    
+}>());
+export const subtractMoney = createAction('[Money] Subtract Money', props<{
+    money: number
+}>());
+export const fetchMoney = createAction('[Money] Fetch Money');
+export const payAddFactorySuccess = createAction('[Money] Pay Add Factory Success', props<{
+    cost: number
+}>());
+export const payAddFactoryFail = createAction('[Money] Pay Add Factory Fail', props<{
+    error: string    
+}>());
+export const payAddFactorySizeSuccess = createAction('[Money] Pay Add Factory Size', props<{
+    cost: number
+}>());
+export const payAddFactorySizeFail = createAction('[Money] Pay Add Factory Size Fail',props<{
+    error: string
+}>());
--- src/app/manufacturing/store/manufacturing.actions.ts
+++ src/app/manufacturing/store/manufacturing.actions.ts
@@ -2,53 +2,24 @@ import { createAction, props } from '@ngrx/store';
 
 import { Factory } from 'src/app/shared/factory.model';
 
-export const setFactories = createAction(
-    '[Manufacturing] Set Factories',
-    props<{
-        factories: Factory[]
-    }>()
-);
-
-export const addFactory = createAction(
-    '[Manufacturing] Add Factory',
-    props<{
-        factory: Factory,
-        cost: number
-    }>()
-);
-
-export const addFactorySuccess = createAction(
-    '[Manufacturing] Add Factory Success'
-);
-
-export const addFactoryFail = createAction(
-    '[Manufacturing] Add Factory Fail'
-);
-
-export const addFactorySize = createAction(
-    '[Manufacturing] Add Factory Size',
-    props<{
-        size: number,
-        index: number,
-        cost: number
-    }>()
-);
-
-export const addFactorySizeSuccess = createAction(
-    '[Manufacturing] Add Factory Size Success'
-);
-
-export const addFactorySizeFail = createAction(
-    '[Manufacturing] Add Factory Size Fail',
-);
-
-export const deleteFactory = createAction(
-    '[Manufacturing] Delete Factory',
-    props<{
-        index: number
-    }>()
-);
-
-export const fetchFactories = createAction(
-    '[Manufacturing] Fetch Factories'
-);
+export const setFactories = createAction('[Manufacturing] Set Factories', props<{
+    factories: Factory[]
+}>());
+export const addFactory = createAction('[Manufacturing] Add Factory',
+props<{
+    factory: Factory,
+    cost: number
+}>());
+export const addFactorySuccess = createAction('[Manufacturing] Add Factory Success');
+export const addFactoryFail = createAction('[Manufacturing] Add Factory Fail');
+export const addFactorySize = createAction('[Manufacturing] Add Factory Size', props<{
+    size: number,
+    index: number,
+    cost: number
+}>());
+export const addFactorySizeSuccess = createAction('[Manufacturing] Add Factory Size Success');
+export const addFactorySizeFail = createAction('[Manufacturing] Add Factory Size Fail',);
+export const deleteFactory = createAction('[Manufacturing] Delete Factory', props<{
+    index: number
+}>());
+export const fetchFactories = createAction('[Manufacturing] Fetch Factories');
