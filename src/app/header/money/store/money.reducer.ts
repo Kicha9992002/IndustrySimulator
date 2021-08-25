693a04c10e66ffda9a72d4a7e37f6a7ea2f1fed0
Kicha <59894683+Kicha9992002@users.noreply.github.com>
2021-08-25 20:39:42 +0200

    Refactoring

--- src/app/header/money/store/money.reducer.ts
+++ src/app/header/money/store/money.reducer.ts
@@ -13,45 +13,30 @@ const initialState: State = {
 const _moneyReducer = createReducer(
     initialState,
 
-    on(
-        MoneyActions.setMoney,
-        (state, action) => ({
-            ...state,
-            money: 0 + action.money
-        })
-    ),
+    on(MoneyActions.setMoney, (state, action) => ({
+        ...state,
+        money: 0 + action.money
+    })),
 
-    on(
-        MoneyActions.addMoney,
-        (state, action) => ({
-            ...state,
-            money: state.money + action.money
-        })
-    ),
+    on(MoneyActions.addMoney, (state, action) => ({
+        ...state,
+        money: state.money + action.money
+    })),
 
-    on(
-        MoneyActions.subtractMoney,
-        (state, action) => ({
-            ...state,
-            money: state.money - action.money
-        })
-    ),
+    on(MoneyActions.subtractMoney, (state, action) => ({
+        ...state,
+        money: state.money - action.money
+    })),
 
-    on(
-        MoneyActions.payAddFactorySuccess,
-        (state, action) => ({
-            ...state,
-            money: state.money - action.cost
-        })
-    ),
+    on(MoneyActions.payAddFactorySuccess, (state, action) => ({
+        ...state,
+        money: state.money - action.cost
+    })),
 
-    on(
-        MoneyActions.payAddFactorySizeSuccess,
-        (state, action) => ({
-            ...state,
-            money: state.money - action.cost
-        })
-    )
+    on(MoneyActions.payAddFactorySizeSuccess, (state, action) => ({
+        ...state,
+        money: state.money - action.cost
+    }))
 );
 
 export function moneyReducer(state: State, action: Action) {
--- src/app/manufacturing/store/manufacturing.reducer.ts
+++ src/app/manufacturing/store/manufacturing.reducer.ts
@@ -27,146 +27,106 @@ const initialState: State = {
 const _manufacturingReducer = createReducer(
     initialState,
 
-    on(
-        ManufacturingActions.setFactories,
-        (state, action) => ({
-            ...state,
-            factories: [...action.factories]
-        })
-    ),
+    on(ManufacturingActions.setFactories, (state, action) => ({
+        ...state,
+        factories: [...action.factories]
+    })),
 
-    on(
-        ManufacturingActions.addFactory,
-        (state, action) => ({
-            ...state,
-            newFactory: action.factory
-        })
-    ),
+    on(ManufacturingActions.addFactory, (state, action) => ({
+        ...state,
+        newFactory: action.factory
+    })),
 
-    on(
-        ManufacturingActions.addFactorySuccess,
-        (state, action) => ({
-            ...state,
-            newFactory: null,
-            factories: state.factories.concat({...state.newFactory})
-        })
-    ),
+    on(ManufacturingActions.addFactorySuccess, (state, action) => ({
+        ...state,
+        newFactory: null,
+        factories: state.factories.concat({...state.newFactory})
+    })),
 
-    on(
-        ManufacturingActions.addFactoryFail,
-        (state, action) => ({
-            ...state,
-            newFactory: null
-        })
-    ),
+    on(ManufacturingActions.addFactoryFail, (state, action) => ({
+        ...state,
+        newFactory: null
+    })),
 
-    on(
-        ManufacturingActions.addFactorySize,
-        (state, action) => ({
-            ...state,
-            editIndex: action.index,
-            editSize: action.size
-        })
-    ),
+    on(ManufacturingActions.addFactorySize, (state, action) => ({
+        ...state,
+        editIndex: action.index,
+        editSize: action.size
+    })),
 
-    on(
-        ManufacturingActions.addFactorySizeSuccess,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            editSize: 0,
-            factories: state.factories.map(
-                (factory, index) => index == state.editIndex ? {...factory, size: factory.size + state.editSize} : factory
-            )
-        })
-    ),
+    on(ManufacturingActions.addFactorySizeSuccess, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        editSize: 0,
+        factories: state.factories.map((factory, index) => 
+            index == state.editIndex ? {...factory, size: factory.size + state.editSize} : factory
+        )
+    })),
 
-    on(
-        ManufacturingActions.addFactorySizeFail,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            editSize: 0
-        })
-    ),
+    on(ManufacturingActions.addFactorySizeFail, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        editSize: 0
+    })),
 
-    on(
-        ManufacturingActions.addEmployee,
-        (state, action) => ({
-            ...state,
-            editIndex: action.factoryIndex,
-            newEmployee: action.employee
-        })
-    ),
+    on(ManufacturingActions.addEmployee, (state, action) => ({
+        ...state,
+        editIndex: action.factoryIndex,
+        newEmployee: action.employee
+    })),
 
-    on(
-        ManufacturingActions.addEmployeeSuccess,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            newEmployee: null,
-            factories: state.factories.map((factory, index) => {
-                if (index == state.editIndex) {
-                    return {...factory, employees: factory.employees.concat({...state.newEmployee})}
-                }
+    on(ManufacturingActions.addEmployeeSuccess, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        newEmployee: null,
+        factories: state.factories.map((factory, index) => {
+            if (index == state.editIndex) {
+                return {...factory, employees: factory.employees.concat({...state.newEmployee})}
+            } else {
                 return factory;
-            })
+            }
         })
-    ),
+    })),
 
-    on(
-        ManufacturingActions.addEmployeeFail,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            newEmployee: null
-        })
-    ),
+    on(ManufacturingActions.addEmployeeFail, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        newEmployee: null
+    })),
 
-    on(
-        ManufacturingActions.removeEmployee,
-        (state, action) => ({
-            ...state,
-            editIndex: action.factoryIndex,
-            employeeIndex: action.employeeIndex
-        })
-    ),
+    on(ManufacturingActions.removeEmployee, (state, action) => ({
+        ...state,
+        editIndex: action.factoryIndex,
+        employeeIndex: action.employeeIndex
+    })),
 
-    on(
-        ManufacturingActions.removeEmployeeSuccess,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            employeeIndex: -1,
-            factories: state.factories.map((factory, index) => {
-                if (index == state.editIndex) {
-                    return {...factory, employees: factory.employees.filter((_, index) => {
-                        return index !== state.employeeIndex;
-                    })};
-                }
+    on(ManufacturingActions.removeEmployeeSuccess, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        employeeIndex: -1,
+        factories: state.factories.map((factory, index) => {
+            if (index == state.editIndex) {
+                return {...factory, employees: factory.employees.filter((_, index) => {
+                    return index !== state.employeeIndex;
+                })};
+            } else {
                 return factory;
-            })
+            }
         })
-    ),
+    })),
 
-    on(
-        ManufacturingActions.removeEmployeeFail,
-        (state, action) => ({
-            ...state,
-            editIndex: -1,
-            employeeIndex: -1
-        })
-    ),
+    on(ManufacturingActions.removeEmployeeFail, (state, action) => ({
+        ...state,
+        editIndex: -1,
+        employeeIndex: -1
+    })),
 
-    on(
-        ManufacturingActions.deleteFactory,
-        (state, action) => ({
-            ...state,
-            factories: state.factories.filter(
-                (_, index) => index !== action.index
-            )
-        })
-    )
+    on(ManufacturingActions.deleteFactory, (state, action) => ({
+        ...state,
+        factories: state.factories.filter((_, index) => 
+            index !== action.index
+        )
+    }))
 );
 
 export function manufacturingReducer(state: State, action: Action) {
