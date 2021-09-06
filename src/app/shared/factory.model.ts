ab3ce422fd7fbdb6faac4bb9fe7656c0c1cc08e4
Kicha <59894683+Kicha9992002@users.noreply.github.com>
2021-09-07 00:14:57 +0200

    added factory types

--- src/app/shared/factory.model.ts
+++ src/app/shared/factory.model.ts
@@ -15,6 +15,14 @@ export class FactoryType {
         'apple Orchard',
         'https://cdn.pixabay.com/photo/2019/02/24/13/05/apple-icon-4017545_1280.png'
     );
+    static readonly cherryOrchard = new FactoryType(
+        'cherry Orchard',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKrDYTIlqKvZFAE3m_pjRrBsMRpnGi46kAQ&usqp=CAU'
+    );
+    static readonly forest = new FactoryType(
+        'forest',
+        'https://cdn.pixabay.com/photo/2019/03/13/04/03/tree-4052262_1280.png'
+    );
 
     private constructor(
         public readonly name: string,
